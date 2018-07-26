6ae7b4b7c0ba9445bc9c923c299df346b1a7a2d8
Rolan Burykin <burykin.rolan@gmail.com>
2018-07-27 00:03:23 +0200

    Initial project structure.

---
+++ src/main/java/rolanb/examples/reactive/ReactiveShopApplication.java
@@ -0,0 +1,12 @@
+package rolanb.examples.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveShopApplication.class, args);
+	}
+}
